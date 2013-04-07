4a59f7bfc7b95f27f784f542012552d8edff71ff
Jed <jed.clinger@buildingenergy.com>
2013-04-07 11:51:49 -0700

    Removes clj-json from dependencies.

--- project.clj
+++ project.clj
@@ -2,7 +2,6 @@
   :description "thin wrapper for Campfire's API"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-http "0.7.0"]
-                 [clj-json "0.5.3"]
                  [cheshire "5.1.0"]]
   :profiles {:dev {:plugins [[lein-midje "3.0.0"]]
                    :dependencies [[midje "1.5.1"]]}})
