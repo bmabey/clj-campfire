e550a521b620ca15f24c39f9e3d4346d9e3ed8ca
Ben Mabey <ben@benmabey.com>
2013-05-03 21:48:35 -0600

    updates project file to lein 2

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject clj-campfire "2.0.0"
+(defproject clj-campfire "2.1.0-SNAPSHOT"
   :description "thin wrapper for Campfire's API"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-http "0.2.7"]
                  [clj-json "0.5.0"]]
-  :dev-dependencies [[midje "1.3.1" :exclusions [org.clojure/clojure]]
-                     [lein-midje "1.0.7"]
-                     [swank-clojure "1.3.4"]])
+  :profiles {:dev
+             {:plugins [[lein-midje "3.0.0"]]
+              :dependencies [
+                             [midje "1.5.0"]
+                             [com.stuartsierra/lazytest "1.2.3"]]}})
