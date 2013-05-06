3bec3d392140f465e7d69ec8d42f3c2fcafa2a8d
Ben Mabey <ben@benmabey.com>
2013-05-06 09:07:44 -0600

    removes lazytest as a dep since I'm not using it for this project

--- project.clj
+++ project.clj
@@ -5,6 +5,4 @@
                  [clj-json "0.5.0"]]
   :profiles {:dev
              {:plugins [[lein-midje "3.0.0"]]
-              :dependencies [
-                             [midje "1.5.0"]
-                             [com.stuartsierra/lazytest "1.2.3"]]}})
+              :dependencies [[midje "1.5.0"]]}})
