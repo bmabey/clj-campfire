adaa28dee36ce3a12ddbeeeccd2d88a160b7d531
Marc O'Morain <marc@circleci.com>
2018-12-10 12:16:57 +0000

    Update dependencies to work with Clojure 1.9
    The following projects have been updated to be compatible with Clojure 1.9:
    
    - org.clojure/core.unify
    - http.async.client
    - midje
    
    The versions in use prior to this change had problems with their use of `ns`
    or `defn` macros that were caught by spec in Clojure 1.9.

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject clj-campfire "2.2.0"
   :description "thin wrapper for Campfire's API"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [http.async.client "0.5.2"]
+                 [http.async.client "1.2.0"]
                  [cheshire "5.1.0"]]
   :profiles {:dev 
-             {:plugins [[lein-midje "3.0.0"]]
-              :dependencies [[midje "1.5.1"]]}})
+             {:plugins [[lein-midje "3.2.1"]]
+              :dependencies [[org.clojure/clojure "1.9.0"]
+                             [midje "1.6.0"]
+                             [org.clojure/core.unify "0.5.7"]]}})
